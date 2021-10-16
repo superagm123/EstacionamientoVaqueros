58c2a4b40346b00882c19bd446edbcba69256bee
Alfonso <alfonso.miramontes@hotmail.com>
2021-10-16 01:21:13 -0500

    Changed the design a little bit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,7 @@ import './About.css';
 const About = () => {
 	return(
 		<section className="bg-light-green container">
-			
+			<h1 className="near-white ml5 title">Sobre Nosotros</h1>
 		</section>
 	);
 }
--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import Car from './Car.png';
-import Car2 from './Car2.png';
 import './Story.css';
 
 const Story = () => {
 	return(
 		<div className="story-container">
-			<div className="image-container">
-				<a href="#">
-					<div className="image">
-						<img alt="Car" src={Car}/>
-					</div>
-				</a>
-				<a href="#">
-					<div className="image image-position">
-						<img alt="Car" src={Car2}/>
-					</div>
+			<div className="image center">
+				<a href="https://www.pexels.com/photo/photography-of-gray-sports-car-919073/" rel="noopener noreferrer nofollow" target="_blank">
+					<img alt="Car" src={Car}/>
 				</a>
 			</div>
-			<div className="tc dib v-top athelas">
-				<h1 className="f1 lh-title">Nuestra Historia</h1>
-				<p className="f3 lh-solid">
+			<div className="tc athelas">
+				<h1 className="f-subheadline lh-title">Nuestra Historia</h1>
+				<p className="f2 lh-solid">
 				  Nullam mattis mauris nec accumsan consectetur. Morbi sed ipsum urna. Ut quis lorem luctus, cursus neque sit amet, imperdiet massa.
 				  Nullam lobortis mauris sit amet enim tristique porta. Pellentesque justo ante, aliquam ut porttitor vel, volutpat non enim. Nullam ac justo magna. Suspendisse porttitor molestie neque vitae pretium. Maecenas cursus est in magna iaculis pretium. Suspendisse vitae felis ac dolor convallis venenatis vitae auctor purus.
 				</p>
