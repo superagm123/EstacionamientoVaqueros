75cd3b21b643f44cff8f40c086678b5b6a36dd3f
Alfonso <alfonso.miramontes@hotmail.com>
2021-10-19 17:59:41 -0500

    added service section

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -7,7 +7,9 @@ const Services = () => {
 			<h1 className="f-subheadline f-subheadline about-title">Sobre Nosotros</h1>
 			<div className="services">
 				<div className="service">
-					
+					<h1 className="service-title"></h1>
+					<i className="service-icon"></i>
+					<p className="service-content"></p>
 				</div>
 			</div>
 		</div>//.container
