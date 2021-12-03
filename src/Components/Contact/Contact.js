44c76d7117cfb29ec489161a9da46b677955aac8
Alfonso <alfonso.miramontes@hotmail.com>
2021-12-03 05:29:39 -0600

    now the costumers can send contact info to the server

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Contact.css'; 
 
-const Contact = () => {
-	return(
-		<React.Fragment>
+class Contact extends Component{
+	constructor(){
+		super();
+		this.state = {
+			name: '',
+			email: '', 
+			message: ''
+		}
+	}
+
+	onNameChange = event => {
+		this.setState({name: event.target.value});
+	}
+
+	onEmailChange = event => {
+		this.setState({email: event.target.value}); 
+	}
+
+	onMessageChange = event => {
+		this.setState({message: event.target.value});
+	}
+
+	cleanEntries = () => {
+		let inputName = document.querySelector("#name");
+		let inputEmail = document.querySelector("#email");
+		let inputMessage = document.querySelector("#message");
+
+		inputName.value = "";
+		inputEmail.value = "";
+		inputMessage.value = "";
+	}
+
+	onSubmitForm = event => {
+		event.preventDefault();
+		const {name, email, message} = this.state;
+		const {cleanEntries} = this;
+		if(name.length > 0 && email.length > 0 && message.length > 0){
+			fetch('http://localhost:3001/send-email', {
+				method: 'post',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					name: name,
+					email: email,
+					message: message 
+				})
+			})
+			  .then(response => response.json())
+			  .then(message => {
+				  	if(message){
+				  		alert('Error al enviar el mensaje');
+				  	}
+			  })
+			  cleanEntries();
+			}else{
+				alert("El formulario no debe estar vacío");
+			}
+	}
+
+	render(){
+		const {onNameChange, onEmailChange, onMessageChange, onSubmitForm} = this;
+		return(
+			<React.Fragment>
 			<div id="contact" className="container mv6 bg-light-green near-white">
 				<h1 className="f-subheadline tc">Contacto</h1>
 				<form>
@@ -21,9 +80,10 @@ const Contact = () => {
 							id="name"
 							className="pa2 input-reset ba
 							hover-bg-black hover-white w-100"
-							type="text"/>
+							type="text"
+							onChange={onNameChange}/>
 						</div>
-						<div className="mv3">
+						<div className="mv3" >
 							<label 
 							className="db fw6 lh-copy f2"
 							htmlFor="email">
@@ -33,7 +93,8 @@ const Contact = () => {
 							id="email"
 							className="pa2 input-reset ba
 							hover-bg-black hover-white w-100"
-							type="email"/>
+							type="email"
+							onChange={onEmailChange}/>
 						</div>
 						<div className="mv3">
 							<label 
@@ -42,9 +103,12 @@ const Contact = () => {
 							Mensaje
 							</label>
 							<textarea 
+							id="message"
 							className="w-100 pa2 ba 
 							hover-bg-black hover-white"
-							rows="10">	
+							rows="10"
+							maxlength="800"
+							onChange={onMessageChange}>	
 							</textarea>
 						</div>
 					</fieldset>
@@ -53,12 +117,14 @@ const Contact = () => {
 						className="b ph3 pv2 input-reset ba b--white w-50 grow 
 						pointer near-white bg-green fw6 f3 dib"
 						type="submit"
-						value="Enviar"/>
+						value="Enviar"
+						onClick={onSubmitForm}/>
 					</div>
 				</form>
 			</div>
 		</React.Fragment>
-	);
+		);
+	}
 }
 
 export default Contact;
